e849faad45fe12d9c2f59d308e9f350899e509c7
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-04-28 16:48:52 +0600

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import {Table} from "antd";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface PropsChangingRow{
+  address1: string,
+  address2: string
+}
+
+const ChangingRow = ({address1, address2}: PropsChangingRow) => {
+  const [address,setAddress] = useState('')
+  let counter = 1;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if(counter%2) setAddress(address1)
+      else setAddress(address2)
+      counter+=1;
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return <div>{address}</div>
+}
+
+const App = () => {
+
+  const dataSource = [
+    {
+      key: '1',
+      name: 'Mike',
+      age: 32,
+      address1: '10 Downing Street 1',
+      address2: 'Street Downing 10 2'
+    },
+    {
+      key: '2',
+      name: 'John',
+      age: 42,
+      address1: '10 Downing Street 3',
+      address2: 'Street Downing 10 4'
+    },
+  ];
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Age',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: 'Address',
+      // @ts-ignore
+      render: (record) => <ChangingRow address1={record.address1} address2={record.address2}/>
+    },
+  ];
+
+  return <Table dataSource={dataSource} columns={columns} />
 }
 
 export default App;
